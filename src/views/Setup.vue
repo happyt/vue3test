d8ac20b390a72f948db010705ead463a47ebdc96
Ian Morton <IanM@Leos-MacBook-Pro-2.local>
2018-10-07 14:58:49 +0100

    Initial version plus vuetify then router

---
+++ src/views/Setup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="setup">
+    <h1>This is the setup page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'setup',
+  components: {
+  }
+}
+</script>
